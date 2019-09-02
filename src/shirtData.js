079ceb00ed72db4d7219312a2a105a820f8b5f71
“hannah6530” <hannah6530@gmail.com>
2019-09-02 11:42:27 -0400

    changes made

--- src/shirtData.js
+++ src/shirtData.js
@@ -1,9 +1,217 @@
 const shirts = [
 {
-  name: ,
-  size: {small, medium, large, extralarge},
-  gender: ,
+  name: 'Akademics Tee' ,
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price: '25.25'
+
+},
+{
+  name: 'Born Fly Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '32.75'
+
+},
+{
+  name: 'Byb Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '15.80'
+
+},
+{
+  name: 'Champion Crop Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '10.00'
+
+},
+{
+  name: 'Champion Oversize Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '15.00'
+
+},
+{
+  name: 'CocaCola Print Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
   price:
 
-}
-]
+},
+{
+  name: 'Drip Too Hard Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price:
+
+},
+{
+  name: 'Fila Print Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price:
+
+},
+{
+  name: 'Fila Talia Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price:
+
+},
+{
+  name: 'Basic Fila Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price:
+
+},
+{
+  name: ,
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price:
+
+},
+{
+  name: 'Golden Ace Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price: '35.00'
+
+},
+{
+  name: 'Hey Arnold Crop Tee',
+  size: {'small', 'medium', 'extra-large'},
+  gender: 'female',
+  price: '20.44'
+
+},
+{
+  name: 'Los Angeles Lakers Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '26.54'
+
+},
+{
+  name: 'Mickey Mouse Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '19.75'
+
+},
+{
+  name: 'Mickey Tee',
+  size: {'small', 'large', 'extra-large'},
+  gender: 'female',
+  price: '22.13'
+
+},
+{
+  name: 'Nasa Print Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price: '30.12'
+
+},
+{
+  name: 'Nascar Racing Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '28.75'
+
+},
+{
+  name: 'Nickelodeon Characters Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price:
+
+},
+{
+  name: 'Gifted Minds Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price: '30.00'
+
+},
+{
+  name: 'Selena Print Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '27.65'
+
+},
+{
+  name: 'Snoopy Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '19.00'
+
+},
+{
+  name: 'Spray Paint Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price:
+
+},
+{
+  name: 'Survival Of The Fittest Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price: '45.00'
+
+},
+{
+  name: 'Comic Book Short Sleeve Shirt',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price: '28.00'
+
+},
+{
+  name: 'Diamond Tour Tee',
+  size: {'small', 'large', 'extra-large'},
+  gender: 'male',
+  price: '10.00'
+
+},
+{
+  name: 'Lrgianni Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price: '24.00'
+
+},
+{
+  name: 'Ying Yang Tee',
+  size: {'medium', 'large', 'extra-large'},
+  gender: 'male',
+  price: '16.99'
+
+},
+{
+  name: 'Rothco Camo Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '16.99'
+
+},{
+  name: 'Filipa Two Layer Crop Tee',
+  size: {'small', 'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '26.99'
+
+},
+{
+  name: 'Alba Fila Crop Tee',
+  size: {'medium', 'large', 'extra-large'},
+  gender: 'female',
+  price: '19.99'
+
+}]
